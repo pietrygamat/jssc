86aad7e512ad3b9d1f551286bc4dc8d2413720ab
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2023-11-14 20:24:03 +0100

    Add same null check for windoof. Add unit test for the null buffer case.

--- src/test/java/jssc/SerialNativeInterfaceTest.java
+++ src/test/java/jssc/SerialNativeInterfaceTest.java
@@ -1,15 +1,23 @@
 package jssc;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class SerialNativeInterfaceTest {
 
+    private SerialNativeInterface testTarget;
+
+    @Before
+    public void before(){
+        testTarget = new SerialNativeInterface();
+    }
 
     @Test
     public void testInitNativeInterface() {
@@ -45,6 +53,18 @@ public class SerialNativeInterfaceTest {
         byte[] buf = new byte[]{ 0x6A, 0x73, 0x73, 0x63, 0x0A };
         SerialNativeInterface testTarget = new SerialNativeInterface();
         testTarget.writeBytes(fd, buf);
+    }
+
+    @Test
+    public void throwsNpeIfPassedBufferIsNull() throws Exception {
+        long fd = 1;
+        byte[] buf = null;
+        try{
+            testTarget.writeBytes(fd, buf);
+            fail("Where is the exception?");
+        }catch( NullPointerException ex ){
+            assertTrue(ex.getMessage().contains("buffer"));
+        }
     }
 
 }
